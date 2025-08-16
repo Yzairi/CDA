0efffcd62cc067947e96d401a7dd426319ff4c67
yassin <yassin.rey200@gmail.com>
2025-08-16 18:17:23 +0200

    feat: update assembly information and add project objectives documentation

--- Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
+++ Back-end/obj/Debug/net8.0/Back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0d382d009ffad0af39520117f7713e37fde5a34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c45b945670d6a7774767cf9420a8d6fe01fe04d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
